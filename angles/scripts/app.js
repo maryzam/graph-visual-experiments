feddb9e26f7e6d62c499e6d15379ab4e502977d6
maryzam <zamo.code@gmail.com>
2019-03-06 23:00:31 +0300

    Create relative angles demo page

--- angles/scripts/app.js
+++ angles/scripts/app.js
@@ -1,3 +1,7 @@
+const radToDec = 180 / Math.PI;
+
+const scaleAngle = d3.scaleSequential(d3.interpolateBlues).domain([90, 0])
+
 const makeDemo = () => {
 
 	const data = generateGraph(25);
@@ -38,6 +42,35 @@ const prepareContainer = (size) => {
 	return container;
 };
 
+const highlightRelativeAngles = (links) => {
+	return (link) => {
+		const baseAngle = calcAngle(link);
+		links.style("stroke", d => scaleAngle(getRelativeAngle(d, baseAngle)));
+	}
+}
+
+const unifyLinks = (links) => {
+	return () => {
+		links
+			.transition()
+				.duration(300)
+			.style("stroke", "black");
+	}
+}
+
+
+calcAngle = (d) => {
+	const x = d.source.x - d.target.x;
+	const y = d.source.y - d.target.y;
+	return Math.atan2(y, x) * radToDec;
+}
+
+const getRelativeAngle = (d, baseAngle) => {
+	const current = calcAngle(d);
+	const dist = Math.abs(current - baseAngle);
+	return (dist > 180) ? (dist - 180) : dist;
+}
+
 const renderGraph = (container, data) => {
 	
 	runSimulation(data);
@@ -52,9 +85,11 @@ const renderGraph = (container, data) => {
 						.attr("x1", d => d.source.x)
 						.attr("y1", d => d.source.y)
 						.attr("x2", d => d.target.x)
-						.attr("y2", d => d.target.y)
-						.style("stroke", "black")
-						.style("opacity", 0.3);
+						.attr("y2", d => d.target.y);
+
+	links
+		.on("mouseover", highlightRelativeAngles(links))
+		.on("mouseout", unifyLinks(links));
 
 	//
 	const nodes = container
@@ -67,16 +102,13 @@ const renderGraph = (container, data) => {
 
 	nodes
 		.append("circle")
-		.attr("r", 2)
-		.style("stroke", "none")
-		.style("fill", "tomato");
-	  
+		.attr("r", 2);
 };
 
 const runSimulation = (data) => {
 
 	const simulation = d3.forceSimulation(data.nodes)
-						      	.force("link", d3.forceLink(data.links).id(d => d.id).distance(50))
+						      	.force("link", d3.forceLink(data.links).id(d => d.id).distance(100))
 						      	.force("charge", d3.forceManyBody())
 						      	.force("x", d3.forceX())
 						     	.force("y", d3.forceY());
