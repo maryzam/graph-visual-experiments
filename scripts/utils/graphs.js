f8514e0cbaeaafbb86779d38d6d1fb78505fc515
maryzam <zamo.code@gmail.com>
2019-03-09 21:57:41 +0300

    fix graph generator

--- scripts/utils/graphs.js
+++ scripts/utils/graphs.js
@@ -1,8 +1,8 @@
-const generateGraph = (nodesCount, probability = 0.1) => {
+const generateGraph = (nodesCount, probability = 0.3) => {
 	const nodes = d3.range(nodesCount).map(d => ({ id: d}));
 	const links = [];
 	for (let source = 0; source < nodesCount; source++) {
-		for (let target = 0; target < nodesCount; target++) {
+		for (let target = source + 1; target < nodesCount; target++) {
 			if (Math.random() < probability) {
 				links.push({ source, target });
 			}
